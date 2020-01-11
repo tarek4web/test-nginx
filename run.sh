219361e87a0c4567e58e67d4946805ef26779e15
Tarek Badawy <tarek2web@gmail.com>
2020-01-11 20:07:36 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;' > mem.out &
 
+ffmpeg -y -reconnect 1 -reconnect_streamed 1 -nostdin -err_detect ignore_err -user_agent $STREAMURLAGENT -i $STREAMURL  $STREAMCODEC  -format hls -sn -hls_time 10 -hls_list_size 5 -hls_flags split_by_time+append_list+discont_start+omit_endlist+delete_segments -hls_segment_filename /usr/share/nginx/html/c_%08d.ts  /usr/share/nginx/html/c.m3u8
+
 python3 /home/run.py
+
