2420f07f4e5963fd10778ca36020ece9ffcb62b9
Tarek Badawy <tarek2web@gmail.com>
2020-01-11 18:56:18 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo -n "configuring nginx"
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 echo -n "starting nginx"
-nginx -g 'daemon off;'
+nginx -g 'daemon off;' > mem.out &
 
 ffmpeg -y -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -nostdin -err_detect ignore_err -user_agent $STREAMURLAGENT -i $STREAMURL -f flv $STREAMCODEC $STREAMRTMP
