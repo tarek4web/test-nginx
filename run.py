d729795a9aab8fc65c38253c518d0bd08d213cbb
Tarek Badawy <tarek2web@gmail.com>
2020-01-11 20:06:55 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,12 @@
 import os
-import argparse
 import sys
 import time
-import signal
-
 
 while 1 > 0:
 
     if len( os.popen( "ps uaxw |grep ffmpeg |grep -c -v grep | awk '{ print $1 }'" ).read().strip().split( '\n' ) ) < 1:
-        try:
-            print ("Restarting ffmpeg")
-            os.system("bash /home/ffmpeg.sh") 
-
-        except (KeyboardInterrupt, SystemExit):
-            print ("CTRL+C received. Killing all workers")
+        print ("Restarting ffmpeg")
+        os.system("bash /home/ffmpeg.sh") 
     else:
-        print ("status stopped sleeping for 5 s")
+        print ("sleeping 5")
         time.sleep(5)
