6eb8bcb47290e4b35082c91a142c1e6e2706f275
Tarek Badawy <tarek2web@gmail.com>
2020-01-11 18:48:08 +0200

    3

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ echo -n "Printing text without newline"
 echo -e "\nRemoving \t special \t characters\n"
 
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+exec /usr/bin/supervisord
+
 
 ffmpeg -y -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -nostdin -err_detect ignore_err -user_agent $STREAMURLAGENT -i $STREAMURL -f flv $STREAMCODEC $STREAMRTMP
