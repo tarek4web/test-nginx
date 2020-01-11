745f83f6b90783fe3a9592cc9da360dcb80d04e0
Tarek Badawy <tarek2web@gmail.com>
2020-01-11 18:52:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo "Printing text"
-echo -n "Printing text without newline"
-echo -e "\nRemoving \t special \t characters\n"
-
+echo -n "configuring nginx"
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-exec /usr/bin/supervisord
-
+echo -n "starting nginx"
+nginx -g 'daemon off;'
 
 ffmpeg -y -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -nostdin -err_detect ignore_err -user_agent $STREAMURLAGENT -i $STREAMURL -f flv $STREAMCODEC $STREAMRTMP
